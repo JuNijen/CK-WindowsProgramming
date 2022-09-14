f20b583a9b8f18cf91d77e82cac190f1987e7f63
Nijen <wjstldms0823@naver.com>
2022-09-14 15:04:25 +0900

    [EDIT] Day3 WM_CHAR Part (p36)
    Signed-off-by: JuNijen <wjstldms0823@naver.com>

--- Project1/First.cpp
+++ Project1/First.cpp
@@ -3,6 +3,8 @@
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 HINSTANCE g_hInst;
 LPCTSTR lpszClass = TEXT("Text Out");
+
+
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
 {
 	HWND hWnd;
@@ -34,26 +36,35 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	}
 	return (int)Message.wParam;
 }
+
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
-	RECT rt = { 100, 100, 300, 300 };
-	const TCHAR* str = TEXT("얼음장 밑에서도" "고기는 헤엄을 치고" "눈보라 속에서도" "매화는 꽃망울을 튼다.");
-		//"절망 속에서도" "삶의 끈기는 희망을 찾고" "사막의 고통 속에서도", "인간은 오아시스의 그늘을 찾는다.");
 
-	switch (iMessage) {
-	case WM_LBUTTONDOWN:
-		hdc = GetDC(hWnd);
-		TextOut(hdc, 100, 100, TEXT("Hello World"), 11);
-		ReleaseDC(hWnd, hdc);
+	static TCHAR str[256];
+	int len = 0;
+
+	switch (iMessage) 
+	{
+	case WM_CHAR:
+		len = lstrlen(str);
+		str[len] = (TCHAR)wParam;
+		str[len + 1] = 0;
+
+		InvalidateRect(hWnd, NULL, FALSE);
 		return 0;
+
+
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
-		TextOut(hdc, 150, 150, TEXT("Hello World"), 11);
-		DrawText(hdc, str, -1, &rt, DT_CENTER | DT_WORDBREAK);
+		TextOut(hdc, 100, 100, str, lstrlen(str));
+
 		EndPaint(hWnd, &ps);
 		return 0;
+
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
