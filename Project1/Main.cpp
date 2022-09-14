37e8796e44b056aee79add8e911ce28349a5078f
Nijen <wjstldms0823@naver.com>
2022-09-14 15:38:18 +0900

    [EDIT] Day3 Drawing dots
    Signed-off-by: JuNijen <wjstldms0823@naver.com>

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -2,7 +2,7 @@
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 HINSTANCE g_hInst;
-LPCTSTR lpszClass = TEXT("Text Out");
+LPCTSTR lpszClass = TEXT("윈도우 프로그래밍");
 
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
@@ -10,10 +10,11 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	HWND hWnd;
 	MSG Message;
 	WNDCLASS WndClass;
-	g_hInst = hInstance;
+	g_hInst = hInstance;	 // 프로그램의 인스턴스 핸들 //(커널이 응용프로그램에 부여한 ID)
 
-	WndClass.cbClsExtra = 0;
-	WndClass.cbWndExtra = 0;
+	WndClass.cbClsExtra = 0; // 윈도우 클래스 데이터 예약 영역
+	WndClass.cbWndExtra = 0; // 윈도우 데이터 예약 영역
+
 	WndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
 	WndClass.hCursor = LoadCursor(NULL, IDC_NO);
 	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
@@ -24,12 +25,24 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	WndClass.style = CS_HREDRAW | CS_VREDRAW;
 	RegisterClass(&WndClass);
 
-	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW | WS_VSCROLL,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL, (HMENU)NULL, hInstance, NULL);
+	// 윈도우 클래스를 등록하고 이 클래스를 기반으로 실제 윈도우를 메모리에 올린다.
+	hWnd = CreateWindow(
+		lpszClass, // 생성되는 윈도우 클래스 이름.
+		TEXT("SIEUN Window Program"), // 윈도우 타이틀 바에 나타나는 문자열
+		WS_OVERLAPPEDWINDOW | WS_VSCROLL,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		(HMENU)NULL,
+		hInstance,
+		NULL);
 
+	//메모리에 생성된 윈도우를 화면에 띄운다.
 	ShowWindow(hWnd, nCmdShow);
 
+	//반복문을 통해 입력 이벤트를 받는다.
 	while (GetMessage(&Message, NULL, 0, 0)) {
 		TranslateMessage(&Message);
 		DispatchMessage(&Message);
@@ -38,59 +51,66 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 }
 
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+
+// WndProc에서 각종 이벤트 진행.
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
 
-	static int x = 100;
-	static int y = 100;
-	static WCHAR cPrint = 'A';
+	RECT rt = { 50, 50, 400, 400 };
 
 
-	switch (iMessage)
+	switch (message)
 	{
-	case WM_KEYDOWN:
-		switch (wParam) 
-		{
-		case VK_LEFT:
-			x -= 8;
-			break;
-		case VK_RIGHT:
-			x += 8;
-			break;
-		case VK_UP:
-			y -= 8;
-			break;
-		case VK_DOWN:
-			y += 8;
-			break;
-		case ' ':
-			if (cPrint == 'A')
-			{
-				cPrint = 'Z';
-			}
-			else
-			{
-				cPrint = 'A';
-			}
-			break;
-		}
-		InvalidateRect(hWnd, NULL, FALSE);
-		return 0;
+		//case WM_KEYDOWN:
+		//	switch (wParam) 
+		//	{
+		//	case VK_LEFT:
+		//		x -= 8;
+		//		break;
+		//	case VK_RIGHT:
+		//		x += 8;
+		//		break;
+		//	case VK_UP:
+		//		y -= 8;
+		//		break;
+		//	case VK_DOWN:
+		//		y += 8;
+		//		break;
+		//	case ' ':
+		//		if (cPrint == 'A')
+		//		{
+		//			cPrint = 'Z';
+		//		}
+		//		else
+		//		{
+		//			cPrint = 'A';
+		//		}
+		//		break;
+		//	}
+		//	InvalidateRect(hWnd, NULL, FALSE);
+		//	return 0;
 
 
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
-		TextOut(hdc, x, y, &cPrint, 1);
+
+		int i;
+		for (i = 0; i < 1000; i++)
+		{
+			SetPixel(hdc, rand() % 400, rand() % 400, RGB(255, 0, 0));
+		}
 
 		EndPaint(hWnd, &ps);
 		return 0;
 
 
 	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
+		PostQuitMessage(0);	// WM_QUIT 메세지를 메시지큐에 넣는다.
+		return 0;			// 직접 사용자가 처리했을 때 0을 돌려주어야 한다.
 	}
-	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+
+	// WndProc에서 처리하지 않은 나머지 메세지들은 윈도우즈 운영체제에게 맡긴다.
+	return (DefWindowProc(hWnd, message, wParam, lParam));
 }
