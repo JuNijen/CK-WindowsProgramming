9278e94057612c10298c405c7a4e45597b43aef5
juNijen <wjstldms0823@naver.com>
2022-11-23 15:28:04 +0900

    [DELETE] Unused files
    Signed-off-by: JuNijen <wjstldms0823@naver.com>

--- WindowsResource/WindowsResource.cpp
+++ WindowsResource/WindowsResource.cpp
@@ -110,15 +110,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	switch (message)
 	{
-
 	case WM_LBUTTONDOWN:
-		//DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG_CPYP), hWnd, DlgProc);
-		//DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG_CALC), hWnd, DlgProcCalc);
-		//DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG_CHECK_DIALOG), hWnd, DlgProcCheckbox);
-		//DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG_SHAPES), hWnd, DlgProcShapes);
-		//DialogBox(hInst, MAKEINTRESOURCE(IDD_LISTBOX), hWnd, DlgListbox);
-		//DialogBox(hInst, MAKEINTRESOURCE(IDD_LISTBOX_COLORPARTY), hWnd, DlgListboxColorParty);
-		//vsDialogBox(hInst, MAKEINTRESOURCE(IDD_LISTBOX_COLORPARTY), hWnd, DlgListboxColorParty);
 		DialogBox(hInst, MAKEINTRESOURCE(IDD_PROC3), hWnd, DlgProc3);
 		break;
 
@@ -130,12 +122,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 		MemDC = CreateCompatibleDC(hdc);
+
 		MyBitmap = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP2));
 		OldBitmap = (HBITMAP)SelectObject(MemDC, MyBitmap);
 		BitBlt(hdc, 0, 0, 279, 178, MemDC, 0, 0, SRCCOPY);
+
 		SelectObject(MemDC, OldBitmap);
 		DeleteObject(MyBitmap);
 		DeleteDC(MemDC);
+
 		EndPaint(hWnd, &ps);
 		return 0;
 	case WM_DESTROY:
@@ -200,312 +195,6 @@ void RGB_Shapes(HDC hdc, int status)
 	SelectObject(hdc, oldP);
 	DeleteObject(hB);
 	DeleteObject(hP);
-}
-
-INT_PTR CALLBACK DlgProc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
-{
-	TCHAR word[100];
-
-	switch (iMsg)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
-		case IDC_BUTTON_COPY:
-			GetDlgItemText(hDlg, IDC_EDIT_ORIGINAL, word, 100);
-			SetDlgItemText(hDlg, IDC_EDIT_COPY, word);
-			break;
-		case IDC_BUTTON_DELETE:
-			SetDlgItemText(hDlg, IDC_EDIT_ORIGINAL, TEXT(" "));
-			SetDlgItemText(hDlg, IDC_EDIT_COPY, TEXT(" "));
-			break;
-		case IDOK:
-			EndDialog(hDlg, 0);
-			break;
-		case IDCANCEL:
-			EndDialog(hDlg, 0);
-			break;
-		}
-	}
-	return (INT_PTR)FALSE;
-}
-
-INT_PTR CALLBACK DlgProcCalc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
-{
-	int numA = GetDlgItemInt(hDlg, IDC_NUMBER_A, NULL, TRUE);
-	int numB = GetDlgItemInt(hDlg, IDC_NUMBER_B, NULL, TRUE);
-
-	switch (iMsg)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
-		case IDC_ADD:
-			SetDlgItemInt(hDlg, IDC_RESULT, numA+numB, TRUE);
-			break;
-		case IDC_SUB:
-			SetDlgItemInt(hDlg, IDC_RESULT, numA - numB, TRUE);
-			break;
-		case IDC_MUL:
-			SetDlgItemInt(hDlg, IDC_RESULT, numA * numB, TRUE);
-			break;
-		case IDC_DIV:
-			SetDlgItemInt(hDlg, IDC_RESULT, numA / numB, TRUE);
-			break;
-		case IDOK:
-			EndDialog(hDlg, 0);
-			break;
-		case IDCANCEL:
-			EndDialog(hDlg, 0);
-			break;
-		}
-	}
-	return (INT_PTR)FALSE;
-}
-
-INT_PTR CALLBACK DlgProcCheckbox(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
-{
-	static int Check[4], Radio;
-	TCHAR hobby[][30] = { TEXT("Music"), TEXT("Book"), TEXT("Game"), TEXT("Exercise") };
-	TCHAR gender[][30] = { TEXT("Male"), TEXT("Female"), TEXT("ETC") };
-	TCHAR output[100];
-
-	switch (iMsg) {
-	case WM_INITDIALOG:
-		CheckRadioButton(hDlg, IDC_RADIO_MALE, IDC_RADIO_FEMALE, IDC_RADIO_ETC);
-		return (INT_PTR)TRUE;
-
-	case WM_COMMAND:
-		switch (LOWORD(wParam)) {
-		case IDC_CHECK_MUSIC:
-			Check[0] = 1 - Check[0];
-			break;
-		case IDC_CHECK_BOOK:
-			Check[1] = 1 - Check[1];
-			break;
-		case IDC_CHECK_GAME:
-			Check[2] = 1 - Check[2];
-			break;
-		case IDC_CHECK_EXERCISE:
-			Check[3] = 1 - Check[3];
-			break;
-
-		case IDC_RADIO_MALE:
-			Radio = 0;
-			break;
-		case IDC_RADIO_FEMALE:
-			Radio = 1;
-			break;
-		case IDC_RADIO_ETC:
-			Radio = 2;
-			break;
-
-		case IDOK:
-			_stprintf_s(output, TEXT("선택한 취미는 %s %s %s %s입니다.\r\n 선택한 성별은 %s 입니다."),
-				Check[0] ? hobby[0] : TEXT(""), Check[1] ? hobby[1] : TEXT(""), Check[2] ? hobby[2] : TEXT(""), Check[3] ? hobby[3] : TEXT(""), gender[Radio]);
-
-			SetDlgItemText(hDlg, IDC_EDIT_OUTPUT, output);
-			break;
-		case IDCANCEL:
-			EndDialog(hDlg, 0);
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
-}
-
-INT_PTR CALLBACK DlgProcShapes(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
-{
-	enum SHAPE
-	{
-		SHAPE_NONE,
-		SHAPE_RECT,
-		SHAPE_CIRCLE,
-	};
-	static SHAPE state;
-
-	switch (iMsg)
-	{
-	case WM_INITDIALOG:
-		state = SHAPE_NONE;
-		return (INT_PTR)TRUE;
-
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
-		case IDC_RADIO_RECT:
-			state = SHAPE_RECT;
-			break;
-		case IDC_RADIO_ELLIPSE:
-			state = SHAPE_CIRCLE;
-			break;
-		case IDOK:
-			EndDialog(hDlg, 0);
-			break;
-		case IDCANCEL:
-			EndDialog(hDlg, 0);
-			break;
-		}
-		InvalidateRect(hDlg, NULL, TRUE);
-
-	case WM_PAINT:
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hDlg, &ps);
-
-		if (state == SHAPE_RECT) { Rectangle(hdc, 10, 10, 100, 100); }
-		else if (state == SHAPE_CIRCLE) { Ellipse(hdc, 10, 10, 100, 100); }
-
-		EndPaint(hDlg, &ps);
-		break;
-	}
-	return (INT_PTR)FALSE;
-}
-
-INT_PTR CALLBACK DlgListbox(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
-{
-	static HWND hList;
-	static int selection;
-	TCHAR name[20];
-
-	switch (iMsg) {
-	case WM_INITDIALOG:
-		hList = GetDlgItem(hDlg, IDC_LIST1);
-		return (INT_PTR)TRUE;
-
-	case WM_COMMAND:
-		switch (LOWORD(wParam)) 
-		{
-		case IDC_JOIN:
-			GetDlgItemText(hDlg, IDC_NAME, name, 20);
-			if (_tcscmp(name, TEXT(""))) SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)name);
-			break;
-
-		case IDC_WITHDRAWAL:
-			SendMessage(hList, LB_DELETESTRING, selection, 0);
-			break;
-
-		case IDC_LIST1:
-			if (HIWORD(wParam) == LBN_SELCHANGE)
-				selection = (int)SendMessage(hList, LB_GETCURSEL, 0, 0);
-			break;
-
-		case IDOK:
-		case IDCANCEL:
-			EndDialog(hDlg, 0);
-			break;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
-}
-
-INT_PTR CALLBACK DlgListboxColorParty(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
-{
-	enum PEN_COLOR
-	{
-		PEN_COLOR_BLACK,
-		PEN_COLOR_RED,
-		PEN_COLOR_BLUE,
-	};
-	enum BRUSH_COLOR
-	{
-		BRUSH_COLOR_BLACK,
-		BRUSH_COLOR_WHITE,
-		BRUSH_COLOR_BLUE,
-	};
-
-	static int pen;
-	static int brush;
-	HDC hdc;
-	HPEN myPen, oldPen;
-	HBRUSH myBrush, oldBrush;
-
-
-	switch(iMsg)
-	{
-	case WM_INITDIALOG:
-		pen = PEN_COLOR_RED;
-		brush = BRUSH_COLOR_BLACK;
-		return (INT_PTR)TRUE;
-
-	case WM_PAINT:
-		PAINTSTRUCT ps;
-		hdc = BeginPaint(hDlg, &ps);
-		myPen = CreatePen(BS_SOLID, 1, RGB(0, 255, 0));
-		myBrush = CreateSolidBrush(RGB(0, 255, 0));
-
-		switch (pen) 
-		{
-		case PEN_COLOR_BLACK:
-			myPen = CreatePen(BS_SOLID, 1, RGB(0, 0, 0));
-			break;
-		case PEN_COLOR_RED:
-			myPen = CreatePen(BS_SOLID, 1, RGB(255, 0, 0));
-			break;
-		case PEN_COLOR_BLUE:
-			myPen = CreatePen(BS_SOLID, 1, RGB(0, 0, 255));
-			break;
-		default:
-			break;
-		}
-
-		switch (brush)
-		{
-		case BRUSH_COLOR_BLACK:
-			myBrush = CreateSolidBrush(RGB(0, 0, 0));
-			break;
-		case BRUSH_COLOR_WHITE:
-			myBrush = CreateSolidBrush(RGB(255, 255, 255));
-			break;
-		case BRUSH_COLOR_BLUE:
-			myBrush = CreateSolidBrush(RGB(0, 0, 255));
-			break;
-		default:
-			break;
-		}
-
-		oldPen = (HPEN)SelectObject(hdc, myPen);
-		oldBrush = (HBRUSH)SelectObject(hdc, myBrush);
-
-		Rectangle(hdc, 12, 120, 284, 200);
-
-		EndPaint(hDlg, &ps);
-		break;
-
-		//SendMessage(hList, CB_GETCURSEL, selection, 0);
-
-	case WM_COMMAND:
-		int id = LOWORD(wParam);
-		//int selection = SendMessage(GetDlgItem(hDlg, id), BM_GETCHECK, 0, 0);
-
-		switch (LOWORD(wParam))
-		{
-		case IDC_R_PEN1:
-			pen = PEN_COLOR_BLACK;
-			break;
-		case IDC_R_PEN2:
-			pen = PEN_COLOR_RED;
-			break;
-		case IDC_R_PEN3:
-			pen = PEN_COLOR_BLUE;
-			break;
-		case IDC_R_BRUSH1:
-			brush = BRUSH_COLOR_BLACK;
-			break;
-		case IDC_R_BRUSH2:
-			brush = BRUSH_COLOR_WHITE;
-			break;
-		case IDC_R_BRUSH3:
-			brush = BRUSH_COLOR_BLUE;
-			break;
-		}
-		InvalidateRect(hDlg, NULL, NULL);
-	}
-	return (INT_PTR)FALSE;
 }
 
 INT_PTR CALLBACK DlgDancingBall(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
@@ -597,11 +286,5 @@ INT_PTR CALLBACK DlgProc3(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		break;
 	}
 
-	return (INT_PTR)FALSE;
-}
-
-
-INT_PTR CALLBACK DlgDancingBall2(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
-{
 	return (INT_PTR)FALSE;
 }
--- WindowsResource/WindowsResource.h
+++ WindowsResource/WindowsResource.h
@@ -8,11 +8,6 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 
 INT_PTR CALLBACK	DlgProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK DlgProcCalc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
-INT_PTR CALLBACK DlgProcCheckbox(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
-INT_PTR CALLBACK DlgProcShapes(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
-INT_PTR CALLBACK DlgListbox(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
-INT_PTR CALLBACK DlgListboxColorParty(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
 INT_PTR CALLBACK DlgDancingBall(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
 INT_PTR CALLBACK DlgProc3(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
 
